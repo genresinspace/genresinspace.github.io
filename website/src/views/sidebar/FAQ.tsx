38c9ee4d9ef4e8b6d5c037ce2bf58f3bc37a85b0
Philpax <me@philpax.me>
2025-07-25 00:17:22 +0200

    feat(website): heavily edit the FAQ

--- website/src/views/sidebar/FAQ.tsx
+++ website/src/views/sidebar/FAQ.tsx
@@ -11,22 +11,14 @@ export function FAQ({ dumpDate }: { dumpDate: string }) {
       answer: (
         <>
           <p>
-            When I'm working on things, I often listen to genre mixes on
-            YouTube; the curation by connoisseurs, especially well-mixed, is
-            sublime. However, I wanted to branch out and listen to genres that I
-            was unfamiliar with, but with the condition that I wanted to explore
-            outwards from what I already knew.
+            When I'm working - on actual work, or on my own projects - I often
+            listen to genre mixes on YouTube; the curation by connoisseurs,
+            especially when well-mixed, is sublime. I can't just listen to the
+            same genres, though: I wanted to branch out and listen to unfamiliar
+            genres, starting from what I already know and working outward.
           </p>
           <p>
-            However, I have a tendency to start projects with too large a scope,
-            which inevitably leads to them never being finished. I didn't want
-            to work my way through a checklist and burn out on music-listening
-            homework.
-          </p>
-          <p>
-            While working on{" "}
-            <ExternalLink href="https://philpax.me">my website</ExternalLink>, I
-            became reacquainted with{" "}
+            When thinking about this predicament, I was reminded of{" "}
             <ExternalLink href="https://eightyeightthirty.one">
               8831
             </ExternalLink>
@@ -36,19 +28,13 @@ export function FAQ({ dumpDate }: { dumpDate: string }) {
             actually finish that would actually solve a problem I actually have:
             I could visualise the graph of genres - or at least the ones English
             Wikipedia knows about - and attach mixes to each genre, so that I
-            could share a little taste of everything with the world (including
-            myself!)
+            could share a little taste of everything with the world.
           </p>
           <p>
-            In addition to that, Wikipedia is one of the greatest resources of
-            our time, and I wanted to do two things: encourage people to improve
-            this little slice of it (I've certainly made a few edits while
-            working on this!) and to draw attention to{" "}
-            <ExternalLink href="https://www.theatlantic.com/technology/archive/2025/02/elon-musk-wikipedia/681577/">
-              the vicious attacks by people who would rather you didn't know any
-              better
-            </ExternalLink>
-            . Don't let them take it from you.
+            More importantly, I love Wikipedia. It's one of humanity's greatest
+            achievements in the domain of knowledge, and I wanted to pay homage
+            by presenting its information through a lens that encourages
+            exploration of another kind.
           </p>
         </>
       ),
@@ -60,22 +46,16 @@ export function FAQ({ dumpDate }: { dumpDate: string }) {
       answer: (
         <>
           <p>
-            This website does not use live data from Wikipedia, because finding
-            all of the genres and the links between them would be unkind to
-            Wikipedia's API.
+            This website does not use live data from Wikipedia; extracting the
+            required information requires looking through every Wikipedia
+            article and how they're connected, and doing that live would be both
+            infeasible and unkind to Wikipedia's servers.
           </p>
           <p>
-            Instead, I used one of Wikipedia's regularly-scheduled database
-            downloads ("dumps"), which allowed me to process every article and
-            link to my heart's content without worrying about being a bad
-            internet citizen.
-          </p>
-          <p>
-            Every once in a while, I will manually update the dump used to the
-            latest version available. I wouldn't mind automating this, but as
-            you might imagine, processing all of Wikipedia requires
-            computational resources that I have access to locally, but that I'd
-            have to pay for if I ran it remotely. Some day, maybe.
+            Instead, I use Wikipedia's regularly-scheduled database snapshots,
+            which allow me to efficiently process every article and link to my
+            heart's content. To keep the data relatively up-to-date, I update
+            the snapshot used roughly every month.
           </p>
         </>
       ),
@@ -85,11 +65,11 @@ export function FAQ({ dumpDate }: { dumpDate: string }) {
       answer: (
         <>
           <p>
-            The English Wikipedia doesn't capture the full breadth of the world
-            of music for a variety of reasons:
+            The English Wikipedia doesn't capture the full breadth of the
+            world's music for a variety of reasons:
           </p>
           <ul className="list-disc pl-5">
-            <li>the genre is poorly known in the English-speaking world</li>
+            <li>the genre is not well-known in the English-speaking world</li>
             <li>nobody has made the time to write an article about it</li>
             <li>
               it's just not notable enough to merit an entire article, or a
@@ -97,26 +77,14 @@ export function FAQ({ dumpDate }: { dumpDate: string }) {
             </li>
             <li>
               it's not treated as a genre unto itself by Wikipedia: for example,
-              "classical music" is too broad to be considered just one genre, so
-              it's not represented here outside of downstream genres
+              "classical music" is too broad to be considered just one genre,
+              which is why it's not represented here outside of the genres that
+              descend from it.
             </li>
           </ul>
           <p>
-            This sucks, but it means that every genre covered here has{" "}
-            <em>some</em> information attached to it to help you contextualise
-            it. In future, I may consider using genres from MusicBrainz and/or
-            other-language Wikis.
-          </p>
-          <p>
-            To see what's <em>really</em> out there, check out{" "}
-            <ExternalLink href="https://musicbrainz.org/genres">
-              MusicBrainz' genres
-            </ExternalLink>
-            , or poke around{" "}
-            <ExternalLink href="https://everynoise.com/">
-              Every Noise At Once
-            </ExternalLink>
-            .
+            This isn't ideal, but it means that every genre has an article
+            attached to it that you can look at for more information.
           </p>
           <p>
             Of course, I encourage you to take matters into your own hands: if
@@ -132,22 +100,60 @@ export function FAQ({ dumpDate }: { dumpDate: string }) {
       answer: (
         <>
           <p>
-            I did, under vibes-based criteria: I looked at the most
-            relevant/most viewed mixes for a given genre, and then picked the
-            mixes that looked like they had the most effort/love/curation. With
-            that being said, however, I also consulted both my own experience
-            and that of friends' to help in selecting representative mixes.
+            I did, using my own judgement: I looked at the most relevant/most
+            viewed mixes for a given genre, and then picked the mixes that
+            looked like they had the most effort/love/curation. With that being
+            said, however, I also consulted both my own experience and that of
+            friends' to help in selecting representative mixes.
           </p>
           <p>
             My selection is not perfect; I had to make judgement calls and to
             apply my own curatorial taste, which can be difficult when I'm not
             acquainted with the genre. For genres that span multiple decades,
             I've tried to pick mixes that cover the most iconic decade, which
-            means there's a bias towards more historical tracks. Sorry!
+            means there's a bias towards mixes featuring older tracks. Sorry!
           </p>
           <p>
-            I'm <em>very</em> open to suggestions for new mixes or replacements
-            - see the FAQ entry below on how to contact me!
+            Something else that's also worth touching upon: I'm an Australian
+            child of the 90s, and while I've done my best to broaden my tastes,
+            I recognise that I'm still very much applying a specific lens to the
+            selections, despite my best efforts. I have no doubt that some of my
+            selections are culturally deficient as a result.
+          </p>
+          <p>
+            With that in mind, I'm <em>very</em> open to suggestions for new
+            mixes or replacements - see the FAQ entry below on how to contact
+            me!
+          </p>
+        </>
+      ),
+    },
+    {
+      question: "How are the top artists for each genre determined?",
+      answer: (
+        <>
+          <p>
+            For every artist on Wikipedia, I find the genres they're associated
+            with, and add them to each of those genres' artist lists. I then
+            score the artists for a given genre using a few metrics, including
+            the number of links to the artists' article, before ranking them and
+            taking the top ten.
+          </p>
+          <p>
+            This allows me to collate the lists automatically using just
+            Wikipedia data, and it works reasonably well, but it also means that
+            people who are well-known for things outside of music may end up
+            receiving a higher rank than is strictly relevant for that genre.
+          </p>
+          <p>
+            This means that, for example, a celebrity that's dabbled in a
+            particular genre may end up receiving a higher rank than an artist
+            who's known for that genre.
+          </p>
+          <p>
+            Unfortunately, this is the only viable solution that uses only
+            Wikipedia data; in future, I may consider using other sources of
+            data to help with this.
           </p>
         </>
       ),
@@ -157,12 +163,9 @@ export function FAQ({ dumpDate }: { dumpDate: string }) {
       answer: (
         <>
           <p>
-            I encourage you to check out other projects of a similar ilk! This
-            project is designed to show you how the genres are connected, and to
-            give you a representative mix for each; but you may find other maps
-            more germane to your interests.
+            I encourage you to check out other projects of a similar ilk! Here
+            are the ones I'm aware of:
           </p>
-          <p>Here's the ones I'm aware of:</p>
           <ul className="list-disc pl-5">
             <li>
               <ExternalLink href="https://everynoise.com/">
@@ -177,70 +180,25 @@ export function FAQ({ dumpDate }: { dumpDate: string }) {
                 Musicmap
               </ExternalLink>
               : an extremely-well-researched, high-quality map of the world of
-              music, with genealogy, purpose-written text, references, and more
+              music, with genealogy, written-for-purpose text, references, and
+              more
             </li>
           </ul>
         </>
       ),
     },
     {
-      question: "Why is it difficult to pan the graph at times?",
-      answer: (
-        <>
-          <p>
-            That's a technical limitation of the solution I'm using for the
-            graph: the labels for each genre block the motion for panning, and
-            there are a *lot* of labels. I'm still working on a way to address
-            this.
-          </p>
-          <p>
-            In the meantime, you may want to consider turning off labels in the
-            settings.
-          </p>
-        </>
-      ),
-    },
-    {
-      question: "What's with the ring of genres around the graph?",
-      answer: (
-        <p>
-          Those are genres that have very few, or zero, connections to the rest
-          of the graph. This can happen because their connections are
-          poorly-documented, they're unique and have few connections to other
-          genres (most often the case with traditional cultural music), or
-          they're too broad to be a distinct entity that other genres can
-          connect to.
-        </p>
-      ),
-    },
-    {
       question:
-        "Why are some genres' descriptions kind of broken, but look fine on Wikipedia?",
-      answer: (
-        <>
-          <p>
-            Wikipedia articles are written in "wikitext". I process the raw
-            wikitext to extract information, and then use my own code to display
-            that wikitext here by roughly replicating Wikipedia's display logic.
-          </p>
-          <p>
-            Unfortunately, my code isn't perfect: it's basically a Minimum
-            Viable Product, and has <em>many</em> rough edges. It especially
-            breaks down around non-English languages (sorry!). Please write to
-            me if you see something egregiously broken!
-          </p>
-        </>
-      ),
-    },
-    {
-      question:
-        "I'd like to reach out to you about a bug/a feature/a music mix/something else. How can I do that?",
+        "What's the best way to reach out to you about a bug, a feature, a mix suggestion, or something else?",
       answer: (
         <p>
           If you have a GitHub account, I'd appreciate you creating an issue in{" "}
-          <ExternalLink href={REPO_LINK}>the repository</ExternalLink>.
-          Otherwise, feel free to contact me through one of the means mentioned
-          on <ExternalLink href="https://philpax.me">my website</ExternalLink>!
+          <ExternalLink href={REPO_LINK}>
+            this project's repository
+          </ExternalLink>
+          . Otherwise, feel free to contact me through one of the means
+          mentioned on{" "}
+          <ExternalLink href="https://philpax.me">my website</ExternalLink>!
         </p>
       ),
     },
